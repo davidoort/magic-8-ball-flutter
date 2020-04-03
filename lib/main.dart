358ca0dbdf796d732c272e3f5562c1e991e33a8e
David Oort Alonso <davidoort@hotmail.com>
2020-04-03 15:38:41 +0200

    finish magic ball challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Text('Ask Me Anything'),
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+            print('Ball number $ballNumber!');
+          },
+        ),
+      ),
+    );
+  }
+}
